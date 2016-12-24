07dd71e4d5b179a08d3fe8d41069bb55e103c0a8
moreners <alexey.morenets@gmail.com>
2016-12-24 14:14:52 +0200

    sum of 40_000_000 ints

--- src/main/java/ThreadApp.java
+++ src/main/java/ThreadApp.java
@@ -1,21 +1,82 @@
+import java.util.Random;
+
 /**
  * Created by a-morenets (alexey.morenets@gmail.com) on 24.12.2016.
  */
 public class ThreadApp {
-    private int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    private Runnable[] threads;
+
+    public static final int ARRAY_SIZE = 40_000_000;
 
     public ThreadApp() {
-
+        process();
     }
 
-    private long sum(int[] arr) {
-        Thread thread = new Thread(new ArrayThread(arr, 0, arr.length));
-        return thread.getSum();
+    private int[] fillArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        shuffle(arr);
+        return arr;
+    }
+
+    private void shuffle(int[] arr) {
+        Random rnd = new Random();
+
+        for (int anArr : arr) {
+            int i1 = rnd.nextInt(arr.length);
+            int i2 = rnd.nextInt(arr.length);
+            swap(arr, i1, i2);
+        }
+    }
+
+    private void swap(int[] arr, int i1, int i2) {
+        int tmp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = tmp;
+    }
+
+    private long linearSum(int[] arr) {
+        long sum = 0;
+        for (int item : arr) {
+            sum += item;
+        }
+        return sum;
+    }
+
+    private long sum(int[] arr, int numParts) {
+        ArrayThread[] arrThreads = new ArrayThread[numParts];
+
+        for (int i = 0; i < numParts; i++) {
+            arrThreads[i] = new ArrayThread(arr, i * arr.length / numParts, arr.length / numParts);
+            Thread thread = new Thread(arrThreads[i]);
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        long sum = 0;
+        for (ArrayThread thrd : arrThreads ) {
+            sum += thrd.getSum();
+        }
+
+        return sum;
+    }
+
+    private void process() {
+        System.out.println("Creating array...");
+        int[] arr = fillArray(ARRAY_SIZE);
+        System.out.println("Linear sum...");
+        System.out.println("LinearSum = " + linearSum(arr));
+        System.out.println("Thread sum...");
+        System.out.println("ThreadSum = " + sum(arr, 4));
+
     }
 
     public static void main(String[] args) {
         ThreadApp app = new ThreadApp();
-        System.out.println();
     }
 }
